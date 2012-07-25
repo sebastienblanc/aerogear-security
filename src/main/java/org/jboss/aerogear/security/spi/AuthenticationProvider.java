1608c08a67ed99d76d7e677ac4d925afdcb507a2
Bruno Oliveira <bruno@abstractj.org>
2012-07-25 14:51:30 -0300

    authentication provider implementation

--- src/main/java/org/jboss/aerogear/security/spi/AuthenticationProvider.java
+++ src/main/java/org/jboss/aerogear/security/spi/AuthenticationProvider.java
@@ -1,7 +1,36 @@
 package org.jboss.aerogear.security.spi;
 
-import org.apache.deltaspike.security.api.User;
+import org.apache.deltaspike.security.api.authorization.annotation.Secures;
+import org.jboss.aerogear.controller.router.Route;
+import org.jboss.aerogear.controller.spi.SecurityProvider;
+import org.jboss.aerogear.security.idm.annotation.CustomSecurityBinding;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public interface AuthenticationProvider {
-    public boolean hasRole(User user, String param);
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class AuthenticationProvider implements SecurityProvider {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthenticationProvider.class);
+
+//    @Inject
+//    private Identity identity;
+//
+//    @Inject
+//    private AuthenticatorManager authenticatorManager;
+
+    @Secures
+    @CustomSecurityBinding
+    public boolean isRouteAllowed(Route route) {
+
+        log.info("============================== AuthenticationProvider ==============================");
+
+        //route.getRoles();
+
+        //log.info("============================== " + route.getRoles().length + " ==============================");
+
+
+        return false;
+    }
 }
