96085ee08e8c5910733b33a6e16eda3461a3d5d2
Bruno Oliveira <bruno@abstractj.org>
2012-10-17 08:28:22 -0300

    Utility class for entity manager injection

---
+++ src/main/java/org/jboss/aerogear/security/util/Resources.java
@@ -0,0 +1,19 @@
+package org.jboss.aerogear.security.util;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.logging.Logger;
+
+public class Resources {
+
+    @Produces
+    @PersistenceContext(unitName = "security-persistence-unit")
+    private EntityManager entityManager;
+
+    @Produces
+    public Logger produceLog(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+    }
+}
