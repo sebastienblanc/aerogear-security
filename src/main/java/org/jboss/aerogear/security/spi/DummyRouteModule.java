ba83983d7ee91d5d7ddee82432a42bbf2915ff40
Bruno Oliveira <bruno@abstractj.org>
2012-10-29 10:26:45 -0200

    Routing dummy module must be alternative

--- src/main/java/org/jboss/aerogear/security/spi/DummyRouteModule.java
+++ src/main/java/org/jboss/aerogear/security/spi/DummyRouteModule.java
@@ -2,6 +2,9 @@ package org.jboss.aerogear.security.spi;
 
 import org.jboss.aerogear.controller.router.AbstractRoutingModule;
 
+import javax.enterprise.inject.Alternative;
+
+@Alternative
 public class DummyRouteModule extends AbstractRoutingModule {
 
     @Override
