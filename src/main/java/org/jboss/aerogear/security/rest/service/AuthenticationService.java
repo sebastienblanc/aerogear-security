d10fb0883e2cffb68b7314ce326484f5f7517856
Bruno Oliveira <bruno@abstractj.org>
2012-11-13 14:51:06 -0200

    First stab at OTP integration

--- src/main/java/org/jboss/aerogear/security/rest/service/AuthenticationService.java
+++ src/main/java/org/jboss/aerogear/security/rest/service/AuthenticationService.java
@@ -27,7 +27,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/auth")
+@Path("/")
 public interface AuthenticationService {
 
     @Path("/otp")
