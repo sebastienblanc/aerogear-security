1b2ce4e1da0bd9c7df6704a234fbd0adb05ccee3
Bruno Oliveira <bruno@abstractj.org>
2013-07-03 17:21:42 -0300

    Fix the error messages

--- src/main/java/org/jboss/aerogear/security/exception/ExceptionHandler.java
+++ src/main/java/org/jboss/aerogear/security/exception/ExceptionHandler.java
@@ -5,28 +5,35 @@ import com.wealdtech.ServerError;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.logging.Logger;
 
-import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
 import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
 
 public class ExceptionHandler {
-
-    private static final Logger LOGGER = Logger.getLogger(ExceptionHandler.class.getSimpleName());
 
     public static void handle(HttpServletResponse response, Exception exception) {
 
         Throwable e = exception.getCause();
 
+
+        if (e instanceof DataError) {
+            responseWriter(response, UNAUTHORIZED.getStatusCode(), e.getMessage());
+        } else if (e instanceof ServerError) {
+            responseWriter(response, INTERNAL_SERVER_ERROR.getStatusCode(), e.getMessage());
+        }
+
+    }
+
+    private static void responseWriter(HttpServletResponse response, int status, String message) {
+
         try {
+
             response.setHeader("WWW-Authenticate", "Hawk");
-            if (e instanceof DataError) {
-                LOGGER.info("Error: " + e.getMessage());
-                response.sendError(UNAUTHORIZED.getStatusCode());
-            } else if (e instanceof ServerError) {
-                LOGGER.info("Error: " + e.getMessage());
-                response.sendError(INTERNAL_SERVER_ERROR.getStatusCode());
-            }
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(message);
+            response.setStatus(status);
+
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
