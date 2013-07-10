cffed50528bf0e6343d86aa0d2198e178ff2416f
Bruno Oliveira <bruno@abstractj.org>
2013-07-10 08:20:27 -0300

    Le missing license headers

--- src/main/java/org/jboss/aerogear/security/web/authz/HawkAuthenticator.java
+++ src/main/java/org/jboss/aerogear/security/web/authz/HawkAuthenticator.java
@@ -1,3 +1,20 @@
+/**
+ * JBoss, Home of Professional Open Source
+ *  Copyright Red Hat, Inc., and individual contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.jboss.aerogear.security.web.authz;
 
 import com.google.common.collect.ImmutableMap;
@@ -8,7 +25,6 @@ import com.wealdtech.hawk.HawkServer;
 import org.jboss.aerogear.security.auth.HawkCredentialProvider;
 
 import javax.inject.Inject;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.InputStreamReader;
@@ -37,7 +53,7 @@ public class HawkAuthenticator {
     }
 
     /**
-     * @param server Hawk HTTP configuration provided by the framework
+     * @param server   Hawk HTTP configuration provided by the framework
      * @param provider developer's credential provider implementation
      */
     @Inject
@@ -47,7 +63,7 @@ public class HawkAuthenticator {
     }
 
     /**
-     * @param request Hawk HTTP <i>Authorization</i> request
+     * @param request  Hawk HTTP <i>Authorization</i> request
      * @param response Hawk HTTP response to authenticated requests with a <i>Server-Authorization</i> header
      * @throws Exception
      */
