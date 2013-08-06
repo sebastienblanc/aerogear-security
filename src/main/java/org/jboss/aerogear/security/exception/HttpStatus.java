3fc9bb79110d3bbe83a580c7afc6c01d7097ad3f
Bruno Oliveira <bruno@abstractj.org>
2013-08-06 14:14:09 -0300

    Message and HTTP status to credential already expired

--- src/main/java/org/jboss/aerogear/security/exception/HttpStatus.java
+++ src/main/java/org/jboss/aerogear/security/exception/HttpStatus.java
@@ -25,7 +25,8 @@ public enum HttpStatus {
     AUTHENTICATION_FAILED("User authentication failed", UNAUTHORIZED.getStatusCode()),
     PASSWORD_RESET_FAILED("Password reset failed", UNAUTHORIZED.getStatusCode()),
     CREDENTIAL_NOT_FOUND("Credentials could not be found", FORBIDDEN.getStatusCode()),
-    ALREADY_LOGGED_IN("Credentials already logged in", NOT_ACCEPTABLE.getStatusCode());
+    ALREADY_LOGGED_IN("Credentials already logged in", NOT_ACCEPTABLE.getStatusCode()),
+    CREDENTIAL_HAS_EXPIRED("Credential has expired", FORBIDDEN.getStatusCode());
 
     private String message;
     private int status;
